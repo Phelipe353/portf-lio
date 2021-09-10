b479183d4272da2787072777974fa6200e2c6391
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2021-09-10 19:52:34 -0300

    att 10/09

---
+++ js/script.js
@@ -0,0 +1,37 @@
+import initModal from './modules/modal.js';
+
+
+function detalhesInicial(){
+  const title = document.querySelector('[data-title]');
+  setTimeout(() => {
+    title.classList.add('ativo');
+  }, 500);
+
+  const piscada = setInterval(()=>{
+    title.classList.toggle('ativo');
+  }, 500)
+
+  setTimeout(() => {
+    clearInterval(piscada);
+    title.classList.remove('ativo');  
+  }, 2500);
+}  
+
+detalhesInicial();
+
+
+function typeWrite(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i)=>{
+    setTimeout(() => {
+      setTimeout(()=>{
+        elemento.innerHTML += letra;
+      }, 75 * i)
+    }, 1000);
+    
+  })
+}
+typeWrite(document.querySelector('.apresentacao h2'));
+
+initModal();
