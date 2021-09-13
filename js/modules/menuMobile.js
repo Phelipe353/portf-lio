38b62ac4037b13b2cc308eb3398bd85bb932f2dc
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2021-09-13 18:39:15 -0300

    13/09 -3

--- js/modules/menuMobile.js
+++ js/modules/menuMobile.js
@@ -3,18 +3,15 @@ import outsideClick from './outsideClick.js';
 export default function initeMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="suave"]');
-  const bgMenu = document.querySelector('header');
   const eventos = ['click'];
 
   if (menuButton) {
     function openMenu(event) {
       menuList.classList.toggle('active');
       menuButton.classList.toggle('active');
-      bgMenu.classList.toggle('active');
       outsideClick(menuList, eventos, ()=>{
         menuList.classList.remove('active');
         menuButton.classList.remove('active');
-        bgMenu.classList.remove('active');
 
       })
     }
