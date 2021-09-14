8b63ec7cb2aa4b7d558ce80e4b95dc247992c6a3
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2021-09-13 21:10:11 -0300

    att 13/09 correcao

--- js/script.js
+++ js/script.js
@@ -1,42 +1,9 @@
 import initModal from './modules/modal.js';
 import initMenuMobile from './modules/menuMobile.js';
+import detalheInicial from './modules/detalheInicial.js';
 
 initModal();
 initMenuMobile();
-
-function detalhesInicial(){
-  const title = document.querySelector('.titulo a');
-  console.log(title);
-  setTimeout(() => {
-    title.classList.add('ativo');
-  }, 500);
-
-  const piscada = setInterval(()=>{
-    title.classList.toggle('ativo');
-  }, 500)
-
-  setTimeout(() => {
-    clearInterval(piscada);
-    title.classList.remove('ativo');  
-  }, 2500);
-}  
-
-detalhesInicial();
-
-
-function typeWrite(elemento){
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  textoArray.forEach((letra, i)=>{
-    setTimeout(() => {
-      setTimeout(()=>{
-        elemento.innerHTML += letra;
-      }, 75 * i)
-    }, 1000);
-    
-  })
-}
-typeWrite(document.querySelector('.apresentacao h2'));
-
+detalheInicial();
 
 
