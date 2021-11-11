d1b82f650f1e5e85dae7ddfa281811f32ac04b6d
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2021-11-10 22:05:28 -0300

    backgorund 1.0

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 import initMenuMobile from './modules/menuMobile.js';
-//import detalheInicial from './modules/detalheInicial.js';
+import detalheInicial from './modules/detalheInicial.js';
 import initScrollSuave from './modules/scrollSuave.js';
 import { SlideNav } from './modules/slide.js';
 
 initMenuMobile();
-//detalheInicial();
+detalheInicial();
 initScrollSuave();
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
